cee0e05e3ef672832234dfb6147dd747211b8711
Mugishab535 <163753503+Mugishab535@users.noreply.github.com>
2025-12-16 23:08:50 +0200

    Refactor model variable name for clarity

--- diabetes-app.py
+++ diabetes-app.py
@@ -1,8 +1,8 @@
-import stramlit as st
+import streamlit as st
 import pickle
 import numpy as np 
 with open('GROUP_05.pkl','rb') as file:
-    model=pickle.load(file)
+    gb_model=pickle.load(file)
     
 st.title("DIABETES PREDICTION")
 st.write("enter the following data")
@@ -16,6 +16,7 @@ DP=st.number_input("DiabetesPedigreeFunction",value=0.0)
 AG=st.number_input("Age",value=0.0)
 if st.button("predict patient diabetes"):
     Input=np.array([[PG,GS,BP,ST,IS,BM,DP,AG]])
-    prediction=model.predict(Input)
-    st.success(pediction[0])
+    prediction=gb_model.predict(Input)
+    st.success(prediction[0])
+
 
